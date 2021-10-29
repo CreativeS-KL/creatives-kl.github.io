260a2558052d85b60889dae570aa24213577c1ff
CreativeS-KL <93375653+CreativeS-KL@users.noreply.github.com>
2021-10-29 14:56:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,32 @@
   <head>
   </head>
   <body>
+    <div class="homebar">
     <a href="">Domov</a>
+    <a href="#členové">Členové</a>
+    <a href="#faq">Faq</a>
+    <a href="#dc-discord">DC / Discord</a>
+    <a href="#ke-stažení">Ke stažení</a>
+    </div>
+    <div class="text"></div>
+    <iframe>
+      #členové
+      <head>
+      </head>
+      <body>
+        <p id="text"></p>
+      </body>
+    </iframe>
+    <style>
+      .homebar {
+        background-image:url();
+        display:inline-flex;
+        text-aling:center;
+      }
+      .text {
+        background-color:black;
+        color:white;
+      }
+    </style>
   </body>
 </html>
